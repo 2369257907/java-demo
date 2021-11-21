cb703c82cced0151fdad0660bd8c5dc711979830
2369257907 <2369257907@qq.com>
2021-11-21 16:18:03 +0800

    使用Callable实现线程

--- java-multiThreaded/src/TestCallable.java
+++ java-multiThreaded/src/TestCallable.java
@@ -10,29 +10,19 @@ public class TestCallable{
 
 
     public static <MyCallable> void main(String[] args) throws ExecutionException, InterruptedException {
-        CallableDemo mc = new CallableDemo("MyCallable");
+        CallableDemo mc = new CallableDemo();
         FutureTask<String> ft = new FutureTask<>(mc);
         Thread thread = new Thread(ft);
-
         thread.start();
         System.out.println(ft.get());
     }
 }
 
 class CallableDemo implements Callable<String>{
-    private int tickets = 5;
-    private String name;
-
     @Override
-    public String call() {
-
-        while (tickets > 0) {
-            tickets--;
-            System.out.println(name + ":售出了第" + (5 - tickets) + "张票");
-        }
-        return this.name+"线程执行结束";
+    public String call(){
+        //...
+        return "线程执行结束";
     }
-
-    public  CallableDemo(String name){this.name=name;}
 }
 
